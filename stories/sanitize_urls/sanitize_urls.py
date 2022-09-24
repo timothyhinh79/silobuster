fdde1429d86d18d8a3f7e939c844d62fab7cc346
Tim <timothyhinh79@gmail.com>
2022-09-23 19:08:51 -0700

    modified code and tests to include logging for different Scenarios

--- stories/sanitize_urls/sanitize_urls.py
+++ stories/sanitize_urls/sanitize_urls.py
@@ -1,32 +1,57 @@
 import re
 import requests
 from url_regex import url_regex
+from urllib.parse import urlparse
+
+# TO-DO's
+# look into changing number of retries for requests.get() to speed it up for non-existent URLs
 
 # extracts all valid URLs from given string
-# valid URLs satisfy the below conditions:
-    # they fit the regular expression (defined in url_regex.py) for identifying URLs
-    # they return a valid 200 status_code response
-def extract_urls(string):
+def sanitize_urls(string):
     url_strings = re.findall(url_regex, string)
+    status = assign_status(string, url_strings)
     
     # extract valid URLs that return a 200 status code out of each matched URL string
-    valid_urls = []
+    url_output = []
     for url_string in url_strings:
-        valid_url = validate_url(url_string)
-        if valid_url: valid_urls.append(valid_url)
+        root_url = extract_root_url(url_string)
+        url_status = assign_url_status(url_string, root_url)
+        # should root URL be in output?
+        url_output.append({'URL': url_string, 'root_URL': root_url})
+        url_output[-1].update(url_status)
+        
+    return {'status': status, 'URLs': url_output}
 
-    return valid_urls
+def assign_status(string, url_strings):
+    if url_strings == []: return 'String contains no URLs'
+
+    if url_strings[0] == string:
+        return 'Entire string is URL'
+    elif len(url_strings) == 1:
+        return 'String contains one URL'
+    else:
+        return 'String contains multiple URLs'
+
+def assign_url_status(url_string, root_url):
+    url_status_code = get_url_status(url_string)
+    output = {'URL_status': url_status_code}
+    if url_status_code == 200:
+        root_url_status_code = 200
+    else:
+        root_url_status_code = get_url_status(root_url)
+    output['root_URL_status'] = root_url_status_code
+    return output
+
 
 # test if given url_string is a valid URL that returns a 200 status code
-# if not, remove characters at the end of the string until we have a valid URL 
-def validate_url(url_string):
-    status_code = requests.get(url_string).status_code
-
-    while re.match(url_regex, url_string) and status_code != 200:
-        url_string = url_string[:-1]
+def get_url_status(url_string):
+    try:
         status_code = requests.get(url_string).status_code
-    
-    if status_code == 200:
-        return url_string
-    else:
-        return None
+    except:
+        return -1
+
+    return status_code
+
+def extract_root_url(url_string):
+    url_parts = urlparse(url_string)
+    return url_parts.scheme + '://' + url_parts.netloc
--- stories/sanitize_urls/tests/test_extract_url.py
+++ stories/sanitize_urls/tests/test_extract_url.py
@@ -1,52 +1,78 @@
-from sanitize_urls import extract_urls, validate_url
+from url_regex import url_regex
+from sanitize_urls import *
 
-def test_validate_url():
+def test_get_url_status():
     extra_characters = 'asdf'
-    test_url = 'https://w3.org/Addressing/URL/url-spec.txt' + extra_characters
+    test_valid_url = 'https://w3.org/Addressing/URL/url-spec.txt'
+    test_invalid_url = test_valid_url + extra_characters
+    nonexistent_url = 'https://nonsenseurlthatdoesntexistz.com/'
 
-    valid_url = validate_url(test_url)
+    valid_status_code = get_url_status(test_valid_url)
+    invalid_status_code = get_url_status(test_invalid_url)
+    nonexistent_status_code = get_url_status(nonexistent_url)
 
-    assert valid_url == 'https://w3.org/Addressing/URL/url-spec.txt'
+    assert valid_status_code == 200
+    assert invalid_status_code == 300
+    assert nonexistent_status_code == -1
 
-def test_extract_urls():
-    test1 = 'URL Specification (https://w3.org/Addressing/URL/url-spec.txt'
-    test2 = 'Possible headstart for validation in JS (https://gist.github.com/dperini/729294'
-    test3 = 'Implements the above in Python (https://miguendes.me/how-to-check-if-a-string-is-a-valid-url-in-python'
+def test_extract_root_url():
+    test_url = 'https://w3.org/Addressing/URL/url-spec.txt'
 
-    assert extract_urls(test1) == ['https://w3.org/Addressing/URL/url-spec.txt']
-    assert extract_urls(test2) == ['https://gist.github.com/dperini/729294']
-    assert extract_urls(test3) == ['https://miguendes.me/how-to-check-if-a-string-is-a-valid-url-in-python']
+    assert extract_root_url(test_url) == 'https://w3.org'
 
-def test_extract_urls_with_multiple_URLs():
-    test1 = 'URL Specification (https://w3.org/Addressing/URL/url-spec.txt'
-    test2 = 'Possible headstart for validation in JS (https://gist.github.com/dperini/729294'
-    test3 = 'Implements the above in Python (https://miguendes.me/how-to-check-if-a-string-is-a-valid-url-in-python'
+# Note, should add assertion for invalid URLs when I figure out how to determine invalid root URLs..
+def test_assign_url_status():
+    test_string_1 = 'https://w3.org/Addressing/URL/url-spec.txt'
+    test_string_2 = 'https://w3.org/Addressing/URL/url-spec.txtasdf'
+    root_url = extract_root_url(test_string_1)
 
-    assert set(extract_urls(test1 + ' ' + test2 + ' ' + test3)) == set([
-        'https://w3.org/Addressing/URL/url-spec.txt',
-        'https://gist.github.com/dperini/729294',
-        'https://miguendes.me/how-to-check-if-a-string-is-a-valid-url-in-python'])
+    assert assign_url_status(test_string_1, root_url) == {'URL_status': 200, 'root_URL_status': 200}
+    assert assign_url_status(test_string_2, root_url) == {'URL_status': 300, 'root_URL_status': 200}
 
-def test_extract_urls_with_extraneous_characters():
-    extra_characters = 'asdf'
-    test1 = 'URL Specification (https://w3.org/Addressing/URL/url-spec.txt' + extra_characters
-    assert extract_urls(test1) == ['https://w3.org/Addressing/URL/url-spec.txt']
+def test_assign_status():
+    test_string_1 = 'https://w3.org/Addressing/URL/url-spec.txt'
+    test_string_1_matches = re.findall(url_regex, test_string_1)
+    test_string_2 = 'URL: ' + test_string_1
+    test_string_2_matches = re.findall(url_regex, test_string_2)
+    test_string_3 = 'random_characters'
+    test_string_3_matches = re.findall(url_regex, test_string_3)
+    test_string_4 = 'https://w3.org/Addressing/URL/url-spec.txt   https://gist.github.com/dperini/729294'
+    test_string_4_matches = re.findall(url_regex, test_string_4)
 
-def test_extract_urls_with_multiple_URLs_with_extraneous_characters():
-    extra_characters = 'asdf'
-    test1 = 'URL Specification (https://w3.org/Addressing/URL/url-spec.txt' + extra_characters
-    test2 = 'Possible headstart for validation in JS (https://gist.github.com/dperini/729294' + extra_characters
-    test3 = 'Implements the above in Python (https://miguendes.me/how-to-check-if-a-string-is-a-valid-url-in-python' + extra_characters
+    assert assign_status(test_string_1, test_string_1_matches) == 'Entire string is URL'
+    assert assign_status(test_string_2, test_string_2_matches) == 'String contains one URL'
+    assert assign_status(test_string_3, test_string_3_matches) == 'String contains no URLs'
+    assert assign_status(test_string_4, test_string_4_matches) == 'String contains multiple URLs'
 
-    assert set(extract_urls(test1 + ' ' + test2 + ' ' + test3)) == set([
-        'https://w3.org/Addressing/URL/url-spec.txt',
-        'https://gist.github.com/dperini/729294',
-        'https://miguendes.me/how-to-check-if-a-string-is-a-valid-url-in-python'])
+
+def test_sanitize_urls_with_only_one_url():
+    test_string_1 = 'https://w3.org/Addressing/URL/url-spec.txt'
+    test_string_2 = 'https://w3.org/Addressing/URL/url-spec.txtasdf'
+    test_string_3 = 'URL Specification https://w3.org/Addressing/URL/url-spec.txt'
+    test_string_4 = 'URL Specification https://w3.org/Addressing/URL/url-spec.txtasdf'
+
+    assert sanitize_urls(test_string_1) == {'status': 'Entire string is URL', 'URLs': [{'URL': 'https://w3.org/Addressing/URL/url-spec.txt', 'root_URL': 'https://w3.org', 'URL_status': 200, 'root_URL_status': 200}]}
+    assert sanitize_urls(test_string_2) == {'status': 'Entire string is URL', 'URLs': [{'URL': 'https://w3.org/Addressing/URL/url-spec.txtasdf', 'root_URL': 'https://w3.org', 'URL_status': 300, 'root_URL_status': 200}]}
+    assert sanitize_urls(test_string_3) == {'status': 'String contains one URL', 'URLs': [{'URL': 'https://w3.org/Addressing/URL/url-spec.txt', 'root_URL': 'https://w3.org', 'URL_status': 200, 'root_URL_status': 200}]}
+    assert sanitize_urls(test_string_4) == {'status': 'String contains one URL', 'URLs': [{'URL': 'https://w3.org/Addressing/URL/url-spec.txtasdf', 'root_URL': 'https://w3.org', 'URL_status': 300, 'root_URL_status': 200}]}
+
+def test_sanitize_urls_with_multiple_URLs():
+    test_string = 'https://w3.org/Addressing/URL/url-spec.txtasdf https://gist.github.com/dperini/729294 https://miguendes.me/how-to-check-if-a-string-is-a-valid-url-in-python'
+
+    assert sanitize_urls(test_string) == {
+        'status': 'String contains multiple URLs', 
+        'URLs': [
+            {'URL': 'https://w3.org/Addressing/URL/url-spec.txtasdf', 'root_URL': 'https://w3.org', 'URL_status': 300, 'root_URL_status': 200},
+            {'URL': 'https://gist.github.com/dperini/729294', 'root_URL': 'https://gist.github.com', 'URL_status': 200, 'root_URL_status': 200},
+            {'URL': 'https://miguendes.me/how-to-check-if-a-string-is-a-valid-url-in-python', 'root_URL': 'https://miguendes.me', 'URL_status': 200, 'root_URL_status': 200}
+        ]
+    }
 
 # INVESTIGATE!!
 # interestingly, http://nonsenseurlthatdoesntexistz.com/ still returns a 200 status code...
-def test_extract_urls_with_valid_URL_string_that_does_not_exist():
-    test_url = 'http://nonsenseurlthatdoesntexistz.com/'
+# it the scheme is https instead of http, the requests.get expectedly errors out
+def test_sanitize_urls_with_valid_URL_string_that_does_not_exist():
+    test_url = 'https://nonsenseurlthatdoesntexistz.com/'
 
-    assert extract_urls(test_url) == []
+    assert sanitize_urls(test_url) == []
 
