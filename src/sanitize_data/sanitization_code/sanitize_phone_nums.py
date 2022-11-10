6a0f6c640ceea68b33763637239111fb19270328
shahabulk <shahabulk@gmail.com>
2022-11-09 18:02:24 -0800

    working on sanitize phone

--- src/sanitize_data/sanitization_code/sanitize_phone_nums.py
+++ src/sanitize_data/sanitization_code/sanitize_phone_nums.py
@@ -8,7 +8,7 @@ from classes.infokind import InfoKind
 #   fill out phone extension if appropriate
 
 # placeholder for now...will spend more time figuring out a better Regex that accurately can identify multiple numbers in a string
-phone_regex = "\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*"
+phone_regex = "\s*(?:\+?(?P<country_code>1{1}))?([-. (]*(?P<area_code>\d{3})[-. )]*)?((?P<first_three>\d{3})[-. ]*(?:[-.x ]*(?P<last_four>\d+))?)\s*"
 
 def get_sanitized_phone_nums_for_update(raw_phone_nums, keys, key_vals, source_table, source_column, logger):
     """Sanitizes the given raw_phone_nums, and returns a JSON with the given keys and sanitized phone numbers
@@ -34,7 +34,7 @@ def get_sanitized_phone_nums_for_update(raw_phone_nums, keys, key_vals, source_t
         table_row_id_str = f"{source_table}.{source_column} with key (" + ', '.join( [f'{col}={val}' for col, val, in zip(keys, key_vals_tuple) ] ) 
 
         # sanitize phone and log any errors or changes resulting from sanitization
-        sanitized_phone = sanitize_phone_num(raw_phone_num, phone_regex, logger, table_row_id_str)
+        sanitized_phone = format_phone_numbers(phone_regex, raw_phone_num)
 
         # add row if sanitization changed raw URL string
         if sanitized_phone != raw_phone_num:
@@ -43,28 +43,67 @@ def get_sanitized_phone_nums_for_update(raw_phone_nums, keys, key_vals, source_t
     
     return rows_w_sanitized_phone
 
-def sanitize_phone_num(raw_phone_num, regex, logger, table_row_id_str = ''):
-    """sanitizes raw phone number to format "(XXX) XXX XXXX"
+# def sanitize_phone_num(raw_phone_num, regex, logger, table_row_id_str = ''):
+#     """sanitizes raw phone number to format "(XXX) XXX XXXX"
 
-    Parameters:
-        raw_phone_num (str): raw phone number string from data table
-        regex (str): pattern for identifying phone numbers
-        logger (logging.RootLogger): logger to output messages on success of sanitization to the terminal for debugging 
-        table_row_id_str (str): string identifying which row in what table is being sanitized
+#     Parameters:
+#         raw_phone_num (str): raw phone number string from data table
+#         regex (str): pattern for identifying phone numbers
+#         logger (logging.RootLogger): logger to output messages on success of sanitization to the terminal for debugging 
+#         table_row_id_str (str): string identifying which row in what table is being sanitized
 
-    Returns:
-         Phone number in format "(XXX) XXX XXXX" if valid phone number is found.
-         If no phone number is found in raw_phone_num, returns original string
-    """
-    try:
-        sanitized_phone = phonenumbers.format_number(phonenumbers.parse(raw_phone_num.strip(), 'US'), phonenumbers.PhoneNumberFormat.NATIONAL).replace('-', ' ')
-        if sanitized_phone != raw_phone_num:
-            logger.debug(f"Sanitized '{raw_phone_num}' to '{sanitized_phone}' in {table_row_id_str}")
-        return sanitized_phone
-    except:
-        logger.error(f"Unable to parse or find phone number in text '{raw_phone_num}' on {table_row_id_str}")
-        return raw_phone_num
+#     Returns:
+#          Phone number in format "(XXX) XXX XXXX" if valid phone number is found.
+#          If no phone number is found in raw_phone_num, returns original string
+#     """
+#     try:
+#         sanitized_phone = phonenumbers.format_number(phonenumbers.parse(raw_phone_num.strip(), 'US'), phonenumbers.PhoneNumberFormat.NATIONAL).replace('-', ' ')
+#         if sanitized_phone != raw_phone_num:
+#             logger.debug(f"Sanitized '{raw_phone_num}' to '{sanitized_phone}' in {table_row_id_str}")
+#         return sanitized_phone
+#     except:
+#         logger.error(f"Unable to parse or find phone number in text '{raw_phone_num}' on {table_row_id_str}")
+#         return raw_phone_num
 
+def pluck_phone_num(regex, raw_phone_str):
+   
+    # sanitized_phones = re.findall(regex, raw_phone_str)
+    # # if not sanitized_phones: return raw_phone_str
+    # # sanitized_phone_str = ', '.join(sanitized_phones) 
+    # numbers = []
+    # if  sanitized_phones[0][0] == '':
+    #     numbers.extend([sanitized_phones[0][2], sanitized_phones[0][4], sanitized_phones[0][5]])
+    # else: numbers.extend([sanitized_phones[0][0], sanitized_phones[0][2], sanitized_phones[0][4], sanitized_phones[0][5]])
+# 
+#
+#
+#
+    phone_number_groups = re.findall(regex, raw_phone_str)
+    number_list = []
+    for groups in phone_number_groups:
+        if groups[0] == '':
+            numbers = ' '.join([groups[2], groups[4], groups[5]])
+        else: 
+            numbers = ' '.join([groups[0], groups[2], groups[4], groups[5]])
+        number_list.append(numbers)
+    return number_list 
+
+
+def format_phone_numbers(regex, raw_phone_num):
+    raw_list = pluck_phone_num(regex, raw_phone_num)
+    sanitized_numbers = []
+    for phone_number in raw_list:
+        if phone_number[0] == 1:
+            added_plus = f'+{phone_number}'
+            sanitized_numbers.append(added_plus)
+    return sanitized_numbers
+        
+    # if raw_list[0] == '1':
+    #     sanitized_phone_number = f'+{raw_list[0]} {raw_list[1]} {raw_list[2]} {raw_list[3]}'
+    # else:
+    #     sanitized_phone_number = f'{raw_list[0]} {raw_list[1]} {raw_list[2]}'
+    # return sanitized_phone_number
+        
 
 # get JSON with key values and sanitized phone number, to be inserted as a row into table on Postgres
 def get_row_w_sanitized_phone(sanitized_phone, keys, key_vals):
