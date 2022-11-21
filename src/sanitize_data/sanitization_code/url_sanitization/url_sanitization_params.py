0d2a7fe528e2c824de7b81caee2f75e9c03e059c
Tim <timothyhinh79@gmail.com>
2022-11-20 19:00:29 -0800

    revised URL sanitization parameters to improve validation checking

--- src/sanitize_data/sanitization_code/url_sanitization/url_sanitization_params.py
+++ src/sanitize_data/sanitization_code/url_sanitization/url_sanitization_params.py
@@ -1,11 +1,11 @@
 
 # number of threads to run sanitize URLs in parallel
-num_threads_default = 200 
+num_threads_default = 500 
 
 # number of seconds for requests.get() to validate if a given URL exists
-requests_timeout_default = 20 
+requests_timeout_default = 60 
 
-# of seconds to reattempt validation of URLs that initially return 429 or 503
+# of seconds before reattempting validation of URLs that initially return 429 or 503
 retry_after_default = 10 
 
 # of maximum attempts to retrieve a valid status code for URLs initially returnning 429 or 503
